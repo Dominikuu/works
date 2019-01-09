5a25de4e33bf606d4917b95d6a101cd3d5662aa6
dominikuu <alanccl92@hotmail.com>
2019-01-09 20:39:25 +0800

    19.01.09
    19.01.09

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+// Modified by Tiago S. Oliveira <TiagoProff@hotmail.com>
+$(document).ready(function () {
+
+    $('[id^="carousel-"]').each(function () {
+        var target = $(this);
+        target.carousel({
+            interval: 4000
+        });
+        target.on("slid", function () {
+            var to_slide;
+            to_slide = target.find(".carousel-item.active").attr("data-slide-no");
+            target.find(".myCarousel-target.active").removeClass("active");
+            target.find(".carousel-indicators [data-slide-to=" + to_slide + "]").addClass("active");
+        });
+        target.find(".myCarousel-target").on("click", function () {
+            //$(this).preventDefault();
+            target.carousel(parseInt($(this).attr("data-slide-to")));
+            target.find(".myCarousel-target.active").removeClass("active");
+            $(this).addClass("active");
+        });
+    });
+});
